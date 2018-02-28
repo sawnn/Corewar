5a48ab4bb9021604e189a1ba4d47897dfb1b4629
Daniel <daniel@localhost.localdomain>
2018-02-28 18:43:42 +0100

    corewar avec les define

--- champions/try_again.s
+++ champions/try_again.s
@@ -1,30 +1,30 @@
-.lol "try_again"
-.ntm "izi"
+.name "try_again"
+.comment "izi"
 
-defense;
-	and *0, *0, r2
-	zjmp *;stg
-	and *0, *0, r2
-	zjmp *;defense
+defense:
+	and %0, %0, r2
+	zjmp %:stg
+	and %0, %0, r2
+	zjmp %:defense
 	
-stg;
-	ld *1, r3
-	ld *0, r5
-	ld *12, r6
-	ld *500, r7
-	ld *197098, r10
-	ld *880, r11
-	ld *66023, r15
-	sti r3, *;defense, *2
-	sti r3, *;defense, *6
-	sti r1, *;prep, *1
-	sti r1, *;live, *1
-	sti r1, *;live, *6
-	sti r1, *;last_live, *1
+stg:
+	ld %1, r3
+	ld %0, r5
+	ld %12, r6
+	ld %500, r7
+	ld %197098, r10
+	ld %880, r11
+	ld %66023, r15
+	sti r3, %:defense, %2
+	sti r3, %:defense, %6
+	sti r1, %:prep, %1
+	sti r1, %:live, %1
+	sti r1, %:live, %6
+	sti r1, %:last_live, %1
 
-	ld *5, r4
-	fork *;prep
-	and *0, *0, r2
+	ld %5, r4
+	fork %:prep
+	and %0, %0, r2
 
 	st r7, 507
 	st r6, 500
@@ -35,23 +35,23 @@ stg;
 	st r3, 400
 	st r6, 393
 
-live;
-	live *0
-	live *0
+live:
+	live %0
+	live %0
 
-loop;
-	zjmp *;live
+loop:
+	zjmp %:live
 
 
-prep;
-	live *0
+prep:
+	live %0
 	st r3, 501
 	st r1, 500
 	st r10, 510
 	st r11, 502
 	st r15, 507
 	st r11, 499
-	fork *;prep
+	fork %:prep
 
-last_live;
-	live *0
+last_live:
+	live %0
