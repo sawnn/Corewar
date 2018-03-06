57140b7408559d845cf1c998c986ae4e2a36a654
Daniel <daniel@localhost.localdomain>
2018-03-06 17:01:36 +0100

    fini

--- asm/src/main.c
+++ asm/src/main.c
@@ -18,8 +18,16 @@ int	help(void)
 
 int	error(int ac, char **av)
 {
+	int i = 0;
+
 	if (ac != 2)
 		return (84);
+	i = strlen_check(av[1], '.');
+	if (i == -1)
+		return (84);
+	if (my_strcmp(&av[i][i], ".s") != 0) {
+		return (84);
+	}
 	return (0);
 }
 
@@ -29,8 +37,6 @@ int	main(int ac, char **av)
 
 	if (error(ac, av) == 84)
 		return (84);
-	if (my_strcmp(av[1], "-h") == 0)
-		return (help());
 	if ((save = parseur(av[1], av[2], 2, NULL)) == NULL)
 		return (84);
 	//faudra free le tableau instruc et t_op *ope)
